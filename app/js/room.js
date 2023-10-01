51dc7b154bb46c911af71078364c352155982c1d
monarxxjain <jainmonark1000@gmail.com>
2023-10-02 02:36:01 +0530

    Changing language

--- app/js/room.js
+++ app/js/room.js
@@ -957,10 +957,14 @@ for (let i = 0; i < audios.length; i++) {
     });
 }
 
-socket.on("answer_message",(msg)=>{
+socket.on("answer_message",async (msg)=>{
     if(remoteSid!==""){
-        document.querySelector(`#${remoteSid} .custom-style-2`).textContent=msg
-        console.log(msg)
+        // let selectedLanguage = languageSelect.value;
+        let selectedLanguage = languageSelect.value;
+        const translationResponse = await fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${selectedLanguage}&dt=t&q=${encodeURIComponent(msg)}`);
+        const translationData = await translationResponse.json();
+        const translation = translationData[0][0][0]
+        document.querySelector(`#${remoteSid} .custom-style-2`).textContent=translation
     }
 })
 
