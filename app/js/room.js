5d9477b60b986cfb706be491096b8bd50143be15
monarxxjain <jainmonark1000@gmail.com>
2023-10-03 16:50:07 +0530

    Try3

--- app/js/room.js
+++ app/js/room.js
@@ -1009,6 +1009,9 @@ if ('webkitSpeechRecognition' in window || 'SpeechRecognition' in window) {
   let selectedLanguage = languageSelect.value;
   languageSelect.addEventListener("click", function() {
     selectedLanguage = languageSelect.value
+    if(siteWidth<955){
+        languageSelect.click()
+    }
     // switch (selectedLanguage) {
     //     case 'en':
     //         recognition.lang = 'en-US'
